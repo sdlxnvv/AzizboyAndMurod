918a9f798133ac62aa1ca26e4e750a858923e383
sdlxnvv <murodsadulloxonov@yandex.ru>
2025-05-15 15:18:57 +0500

    commit by murod

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // by azizboy
         System.out.println("Hello World!");
+
+        // by murod
         System.out.println("Hello World 2");
     }
 }
